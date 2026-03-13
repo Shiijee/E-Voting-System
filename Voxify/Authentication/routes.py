33aab495499e8758a0ca26747be24240ea60423b
Mintzuki git config --global user.email jamesmatthewalmonte50@gmail.comgit config --global user.name Mintzuki <jamesmatthewalmonte50@gmail.com>
2026-03-13 18:09:34 +0800

    addition

--- Voxify/Authentication/routes.py
+++ Voxify/Authentication/routes.py
@@ -1,19 +1,25 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from Voxify import mysql
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
-@auth_bp.route("/", methods=["GET","POST"])
+@auth_bp.route("/login", methods=["GET","POST"])
 def login():
 
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
 
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s",(username,password))
+        conn = current_app.config["get_db_connection"]()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE username=%s AND password=%s",
+            (username, password)
+        )
+
         user = cursor.fetchone()
         cursor.close()
+        conn.close()
 
         if user:
             role = user[5]
