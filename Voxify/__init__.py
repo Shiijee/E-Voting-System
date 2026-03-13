80c26323c226a06ad7def5f378384812c1ba50e6
Mintzuki git config --global user.email jamesmatthewalmonte50@gmail.comgit config --global user.name Mintzuki <jamesmatthewalmonte50@gmail.com>
2026-03-13 17:17:59 +0800

    init

--- Voxify/__init__.py
+++ Voxify/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+import mysql.connector
 
-mysql = MySQL()
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="evoting_db"
+    )
+    return connection
+
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'evoting_db'
-
-    mysql.init_app(app)
 
     # Register modules
     from Voxify.Admin.routes import admin_bp
@@ -21,5 +22,8 @@ def create_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp, url_prefix="/admin")
     app.register_blueprint(voter_bp, url_prefix="/voter")
+
+    # make db accessible in other files
+    app.config["get_db_connection"] = get_db_connection
 
     return app
