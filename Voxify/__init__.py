9e14159204436654457582a02cbc41f560a56c67
Mintzuki git config --global user.email jamesmatthewalmonte50@gmail.comgit config --global user.name Mintzuki <jamesmatthewalmonte50@gmail.com>
2026-03-13 16:48:32 +0800

    database = 'evoting_db'

--- Voxify/__init__.py
+++ Voxify/__init__.py
@@ -1,1 +1,8 @@
 from flask import Flask
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'evoting_db',
+    'auth_plugin': 'mysql_native_password'}
